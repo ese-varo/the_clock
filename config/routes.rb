5e72d185d6ec8a5ded1de4edb50cf18038f2f966
Alvaro Padilla <varoboss.ap@gmail.com>
2021-02-17 13:50:04 -0600

    Add routes for each resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :users do
+    resources :timezones, except: [:index]
+    resources :stopwatches
+    resources :alarms
+  end
+
   root to: 'clock#index'
+  get 'all_timezones',      to: 'clock#timezones'
+  get 'stopwatch',          to: 'clock#stopwatch'
+  get 'favorite_timezones', to: 'timezones#index'
 end
