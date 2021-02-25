ee4cbfc452f9b9cd8eac7ff2e6da1b21cd007fb5
Gregorio <gregoriovazya@gmail.com>
2021-02-24 18:06:58 -0700

    Use callbacks on stopwatch index view

--- app/models/stopwatch.rb
+++ app/models/stopwatch.rb
@@ -1,3 +1,9 @@
 class Stopwatch < ApplicationRecord
+  after_find :change_data
   belongs_to :user
+
+  private
+  def change_data
+    self.time = "#{sprintf("%.2d", (time.to_i % (1000 * 60 * 60)) / (1000 * 60))}:#{sprintf("%.2d", (time.to_i % (1000 * 60)) / 1000)}"
+  end
 end
